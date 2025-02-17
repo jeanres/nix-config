bf5830a69efb956b3a92088bb3ca909dcef6f13f
Jeanre Swanepoel <jeanres@icloud.com>
2025-02-17 09:35:34 +0200

    cleanup

--- home/neovim/lua/plugins/colorscheme.lua
+++ home/neovim/lua/plugins/colorscheme.lua
@@ -1,8 +1,19 @@
--- lua/plugins/rose-pine.lua
 return { 
-	"rose-pine/neovim", 
-	name = "rose-pine",
-	config = function()
-		vim.cmd("colorscheme rose-pine")
-	end
+    "rose-pine/neovim", 
+    name = "rose-pine",
+    config = function()
+	require("rose-pine").setup({
+	    enable = {
+		terminal = true,
+		legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
+	    },
+
+	    styles = {
+		bold = true,
+		italic = true,
+		transparency = false,
+	    },
+	})
+	vim.cmd("colorscheme rose-pine")
+    end
 }
