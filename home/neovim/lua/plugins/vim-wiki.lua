a0428916d2b90701ea5280e49638af2458abab17
Jeanre Swanepoel <jeanres@icloud.com>
2025-04-18 18:20:06 +0200

    remove markdown

--- home/neovim/lua/plugins/vim-wiki.lua
+++ home/neovim/lua/plugins/vim-wiki.lua
@@ -1,18 +1,13 @@
 return {
-  -- The plugin location on GitHub
   "vimwiki/vimwiki",
-  -- The event that triggers the plugin
   event = "BufEnter *.md",
-  -- The keys that trigger the plugin
   keys = { "<leader>ww", "<leader>wt" },
-  -- The configuration for the plugin
   init = function()
     vim.g.vimwiki_list = {
       {
         -- Here will be the path for your wiki
         path = "/Users/jeanre.swanepoel/Documents/vimwiki",
         -- The syntax for the wiki
-        syntax = "markdown",
         ext = "wiki",
       },
     }
